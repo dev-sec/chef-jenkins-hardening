37e85e2af8fe7e7a958481673b9ee5ac98a6e352
Christoph Hartmann <chris@lollyrock.com>
2015-04-16 19:51:58 +0200

    update proxy recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,5 +17,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'jenkins-hardening::proxy'
 include_recipe 'jenkins-hardening::jenkins'
+include_recipe 'jenkins-hardening::proxy'
